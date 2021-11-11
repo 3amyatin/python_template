a247d569e7d2a83bf613b4e335f513f2c556bcbd
Dmitry <digy@digy.ru>
2021-11-11 11:57:06 +0100

    +pprint

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+import re
 import zipfile
+import pprint
+printer = pprint.PrettyPrinter(indent=2, width=40, sort_dicts=False)  # printer.pprint(etwas)
 
 import requests
 import numpy as np
@@ -7,6 +10,7 @@ import pandas as pd
 import seaborn as sns
 %matplotlib inline
 import matplotlib.pyplot as plt
+
 
 
 
